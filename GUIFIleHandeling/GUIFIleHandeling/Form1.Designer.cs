89be1291297bfbfa1fe3ddc32203a72252e047f0
yxstagrabben <67810043+yxstagrabben@users.noreply.github.com>
2022-03-24 09:10:08 +0100

    Add files via upload

---
+++ GUIFIleHandeling/GUIFIleHandeling/Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace GUIFIleHandeling
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.printDocument1 = new System.Drawing.Printing.PrintDocument();
+            this.SubmitButton = new System.Windows.Forms.Button();
+            this.displayData = new System.Windows.Forms.ListBox();
+            this.inputBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // SubmitButton
+            // 
+            this.SubmitButton.Location = new System.Drawing.Point(30, 85);
+            this.SubmitButton.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.SubmitButton.Name = "SubmitButton";
+            this.SubmitButton.Size = new System.Drawing.Size(67, 23);
+            this.SubmitButton.TabIndex = 0;
+            this.SubmitButton.Text = "Submit";
+            this.SubmitButton.UseVisualStyleBackColor = true;
+            this.SubmitButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // displayData
+            // 
+            this.displayData.FormattingEnabled = true;
+            this.displayData.Location = new System.Drawing.Point(370, 46);
+            this.displayData.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.displayData.Name = "displayData";
+            this.displayData.Size = new System.Drawing.Size(81, 121);
+            this.displayData.TabIndex = 1;
+            this.displayData.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // inputBox
+            // 
+            this.inputBox.Location = new System.Drawing.Point(30, 46);
+            this.inputBox.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.inputBox.Name = "inputBox";
+            this.inputBox.Size = new System.Drawing.Size(68, 20);
+            this.inputBox.TabIndex = 2;
+            this.inputBox.KeyDown += new System.Windows.Forms.KeyEventHandler(this.inputBox_KeyDown);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(516, 289);
+            this.Controls.Add(this.inputBox);
+            this.Controls.Add(this.displayData);
+            this.Controls.Add(this.SubmitButton);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Drawing.Printing.PrintDocument printDocument1;
+        private System.Windows.Forms.Button SubmitButton;
+        private System.Windows.Forms.ListBox displayData;
+        private System.Windows.Forms.TextBox inputBox;
+    }
+}
+
---
+++ GUIFIleHandeling/GUIFIleHandeling/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace GUIFIleHandeling
+{
+    public partial class Form1 : Form
+    {
+        string sTextBox;
+        string filePath = "data.csv";
+        int iLine;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            if (!File.Exists(filePath))
+            {
+                File.Create(filePath);
+            }
+            displayFileContents();
+        }
+        private void displayFileContents()
+        {
+            using(var reader = new StreamReader(filePath))
+            {
+                while(!reader.EndOfStream)
+                {
+                    var line = reader.ReadLine();
+                    var values = line.Split(',');
+                    displayData.Items.Add(values[iLine]);
+                    iLine += 1; 
+                }
+            }
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var csv = new StringBuilder();
+            try
+            {
+                sTextBox = inputBox.Text;
+                if (sTextBox != null)
+                {
+                    inputBox.Clear();
+                    displayData.Items.Add(sTextBox);
+                    csv.AppendLine(sTextBox);
+                    File.WriteAllText(filePath, csv.ToString());
+                }
+                else
+                {
+
+                }
+            }
+            catch(Exception ex)
+            {
+
+            }
+            finally
+            { 
+            }
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void inputBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                button1_Click(this, new EventArgs());
+            }
+        }
+    }
+}
